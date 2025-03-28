e0340a552931bc4cacdf57df5c2069e801a736e4
dededc37 <134894425+dededc37@users.noreply.github.com>
2025-03-28 16:27:23 -0300

    resolvendo bugs no js

--- script.js
+++ script.js
@@ -11,9 +11,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         e.preventDefault();
         const target = document.querySelector(this.getAttribute('href'));
         if (target) {
-            target.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
+            const headerOffset = 80; // Ajustado para 80px
+            const elementPosition = target.getBoundingClientRect().top;
+            const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+            window.scrollTo({
+                top: offsetPosition,
+                behavior: 'smooth'
             });
         }
     });
